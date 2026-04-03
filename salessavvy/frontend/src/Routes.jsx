cdd70531a1c71d8066c42bcba5181be6941df14b
supritambig@gmail.com <supritambig@gmail.com>
2026-04-03 19:27:08 +0530

    Refactor Dockerfile to remove unnecessary quiet flag in Maven build command and update Routes.jsx import statement for clarity

--- salessavvy/frontend/src/Routes.jsx
+++ salessavvy/frontend/src/Routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState } from 'react';
 import LoginPage from "./LoginPage";
 import RegistrationPage from "./RegistrationPage";
 import CustomerHomePage from "./CustomerHomePage";
