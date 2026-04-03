3b41fe2110d0fffe11d5ee0a60514cc140def2b7
supritambig@gmail.com <supritambig@gmail.com>
2026-04-03 23:40:42 +0530

    dockerfilee

--- salessavvy/frontend/src/Routes.jsx
+++ salessavvy/frontend/src/Routes.jsx
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-
+import { useNavigate } from 'react-router-dom';
 import LoginPage from "./LoginPage";
 import RegistrationPage from "./RegistrationPage";
 import CustomerHomePage from "./CustomerHomePage";
